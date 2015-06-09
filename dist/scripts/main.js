01ecc23e621bfa7b9da80a46d7acec42ecf19496
Joshua Lagrange <jlagrange87@gmail.com>
2015-06-09 15:40:32 -0500

    Finished

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -8,35 +8,26 @@ $(document).ready(function(){
 
 	})
 
-	// creating a new item collection
 	var PhotoList = new PhotoCollection();
 	PhotoList.fetch();
-	// creating a new template
 	var PhotoBuilder = _.template($("#photoTemplate").html());
 
-	// listening for a submit event
 	$("#photoSubmitForm").submit(function(e){
 		e.preventDefault();
-		// creating a new item model
 		var newPhoto = new ListPhoto({
 			photo: $("#photoUpload").val(),
 			comment: $("#photoComment").val()
 		});
-		// adding new todo item into the list colleciton.
 		if(newPhoto.isValid()){
 			PhotoList.add(newPhoto);
 			console.log(PhotoList);
-			// newPhoto.save();
+			newPhoto.save();
 			$(".dropdown-form").slideUp("slow");
 		}
 		else {
 			alert("NO!")
 		}
-		
-		
 	});
-
-	// listening for the add event to display on html.
 	PhotoList.on("add", function(model){
 		var photoHtml = PhotoBuilder(model.attributes);
 		$("#my-photos").append(photoHtml);
